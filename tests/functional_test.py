22d810835de5efbdbd242bd857e33c8b8e6e00ab
levilightbeam <levi@lightbeam.ai>
2025-02-15 22:59:41 +0700

    done

--- tests/functional_test.py
+++
@@ -1,8 +0,0 @@
-from utils.data_utils import load_data_from_excels
-
-
-class TestRules:
-    def test_load_data(self):
-        files = ["predata.xls", "Data.xlsx"]
-        data = load_data_from_excels(files)
-        pass
